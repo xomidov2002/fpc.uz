16b9570e9db695218e9e117c992312794dbe4739
xomidov2002 <husniddinxomidov2002@gmail.com>
2024-10-16 16:21:23 +0500

    added meta tags

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import { createHead } from '@vueuse/head';
 import { createI18n } from 'vue-i18n'
 import uz from './locale/uz.json'
 import ru from './locale/ru.json'
 
 const app = createApp(App)
+const head = createHead()
 
 const i18n = createI18n({
   legacy: false,
@@ -21,5 +23,6 @@ const i18n = createI18n({
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
+app.use(head)
 
 app.mount('#app')
