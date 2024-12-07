06f049255d4bf161bc4921051d460081ffe5876a
xomidov2002 <husniddinxomidov2002@gmail.com>
2024-12-07 17:56:46 +0500

    changes contact section

--- src/views/home/contact.vue
+++ src/views/home/contact.vue
@@ -88,14 +88,15 @@ const sendMessage = async () => {
 </script>
 
 <template>
-  <div class="bg-[url('/contact.png')] bg-no-repeat bg-center bg-fixed bg-cover">
+  <div class="bg-no-repeat bg-center bg-cover">
     <div class="container mx-auto px-5 py-11">
       <p
         class="text-xl sm:text-xl md:text-2xl lg:text-3xl font-semibold text-white px-5 border-l-2 py-5 border-l-blue-600 mb-5"
       >
         {{ t('mainPage.contact') }}
       </p>
-      <div class="w-full p-5 flex flex-wrap lg:flex-nowrap lg:justify-evenly items-center relative">
+      <div class="w-full bg-[url('/contact.jpg')] overflow-hidden bg-cover bg-fixed bg-center bg-no-repeat  p-5 flex flex-wrap lg:flex-nowrap lg:justify-evenly items-center relative border-2 border-[#2563EB] rounded-2xl">
+        <div class="absolute w-full h-full bg-black/80"></div>
         <div class="p-4">
           <div class="form-container bg-none rounded-2xl">
             <div class="form">
@@ -124,7 +125,7 @@ const sendMessage = async () => {
               <!-- Notification Messages -->
               <transition name="fade">
                 <div
-                  class="absolute uppercase -bottom-[30%] -right-[11%] p-5 border-r-8 border-r-[#080D75] bg-[#14151B]"
+                  class="absolute border border-l-[#080D75] border-white p-3 bottom-0 -right-10 bg-[#14151B]"
                   v-if="showErrorMessage || showSuccessMessage"
                 >
                   <p v-if="showErrorMessage" class="text-red-500">{{ errorMessage }}</p>
@@ -134,21 +135,21 @@ const sendMessage = async () => {
             </div>
           </div>
         </div>
-        <div class="pl-56">
-          <p class="font-[semibold] text-[#292A2E] text-2xl tracking-widest pb-11">
+        <div>
+          <p class="font-[semibold] text-white text-2xl tracking-widest pb-11">
             {{ t('mainPage.contact') }}
           </p>
           <div class="flex items-center gap-5">
             <BaseIcon name="message" class="w-10 h-10 text-[#080D75]" />
-            <p class="text-[#292A2E] tracking-widest">example@gmail.com</p>
+            <p class="text-white tracking-widest">example@gmail.com</p>
           </div>
           <div class="flex py-5 items-center gap-5">
             <BaseIcon name="phone" class="w-10 h-10 text-[#080D75]" />
-            <p class="text-[#292A2E] tracking-widest">+998 93 378 63 61</p>
+            <p class="text-white tracking-widest">+998 93 378 63 61</p>
           </div>
           <div class="flex items-center gap-5">
             <BaseIcon name="location" class="w-10 h-10 text-[#080D75]" />
-            <p class="text-[#292A2E] tracking-widest">
+            <p class="text-white tracking-widest">
               Toshkent, Chilonzor tumani Olmazor massivi 15/10
             </p>
           </div>
