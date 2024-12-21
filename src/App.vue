5167639d27062c7a6d45c5a73a05ede73a7594db
xomidov2002 <husniddinxomidov2002@gmail.com>
2024-12-21 18:02:05 +0500

    changed meta tags2

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,15 @@ useSeoMeta({
   ogTitle: 'Professional xizmatlar - marketing, moliyaviy tahlil, veb-ishlab chiqish va boshqalar',
   description: "Marketing, moliyaviy hisob va tahlil, buxgalteriya hisobi, texnik-iqtisodiy asoslash, biznesni avtomatlashtirish, veb va ilovalarni ishlab chiqish bo'yicha ekspert xizmatlari. Bizning kompleks yechimlarimiz bilan biznesingiz samaradorligini oshiring.",
   ogDescription: "Biz marketing, moliyaviy tahlil, buxgalteriya hisobi, texnik-iqtisodiy asoslash, biznesni avtomatlashtirish, veb va ilovalarni ishlab chiqish bo'yicha ekspert xizmatlarini taqdim etamiz. Bizning kompleks yechimlarimiz bilan biznesingiz samaradorligini oshiring.",
-  ogImage: '/logo-1.png',
-  ogUrl: 'https://fpcuz.netlify.app/',
+  ogImage: '/fpc.png',
+  ogUrl: 'https://fullpoint.uz',
   keywords: "marketing, moliyaviy hisob, biznes tahlili, buxgalteriya hisobi, texnik-iqtisodiy asoslash, savdo bo'limi, biznesni avtomatlashtirish, veb-ishlab chiqish, ilovalarni ishlab chiqish, biznesni tizimlashtirish, professional xizmatlar, маркетинг, финансовый учет, бизнес-анализ, бухгалтерия, технико-экономическое обоснование, отдел продаж, автоматизация бизнеса, веб-разработка, разработка приложений, систематизация бизнеса, профессиональные услуги",
 });
 
 // Additional head configuration
 useHead({
   link: [
-    { rel: 'icon', href: '/logo-1.png' },
+    { rel: 'icon', href: '/favicon.ico' },
     {
       rel: 'stylesheet',
       href: 'https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap',
