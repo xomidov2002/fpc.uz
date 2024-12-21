a3ac057b36146968a06b975179c25631c49aad1e
xomidov2002 <husniddinxomidov2002@gmail.com>
2024-12-21 16:46:39 +0500

    changed meta tags

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './assets/main.css'
-import { createApp } from 'vue'
+import { createApp, onMounted } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
